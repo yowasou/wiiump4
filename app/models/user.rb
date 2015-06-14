89cfe9499c582beba8d5a9f6bd4e298c0d053c40
yowasou <jun.makita@gmail.com>
2015-06-14 21:41:28 +0900

    動画ファイルこわれる

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+  def encryptor
+    secret = ENV["ENC_KEY"]
+    ::ActiveSupport::MessageEncryptor.new(secret)
+  end
+
+  def icon=(val)
+    encryptor = self.encryptor
+    write_attribute("icon",encryptor.encrypt_and_sign(val))
+  end
+
+  def icon
+    encryptor = self.encryptor
+    encryptor.decrypt_and_verify(read_attribute("icon"))
+  end
 end
